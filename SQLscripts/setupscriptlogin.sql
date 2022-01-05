c6aa41fea297399060b47d9eb6ea7dad283da131
Sam Marrello <sammarrello@gmail.com>
2022-01-05 13:27:09 -0500

    More work on login pages

---
+++ SQLscripts/setupscriptlogin.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee (
+    id INT,
+    employeeNameFirst VARCHAR(255),
+    employeeNameLast VARCHAR(255),
+    employeeEmail VARCHAR(255),
+    employeeNumber INT,
+    employeePassword VARCHAR(255),
+    PRIMARY KEY (id, employeeNumber)
+);
